59c6621856979cead049296f71cc9856ee0887f0
Marvicss <marconivictor15@gmail.com>
2024-08-12 20:37:55 -0300

    atividade estudo dirigido

---
+++ src/Componente.js
@@ -0,0 +1,15 @@
+
+function Componente() {
+  return (
+    <div style={{ textAlign: 'center', margin: '20px 0' }}>
+      <img
+        src="https://cdn.folhape.com.br/upload/dn_arquivo/2024/02/captura-de-tela-2024-02-06-083122.png"
+        alt="Placeholder"
+        style={{ width: '150px', height: '150px' }}
+      />
+      <p>Exemplo de componente reutilizavel</p>
+    </div>
+  );
+}
+
+export default Componente;
